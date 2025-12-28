32b0b063206396b3200159c26b2097ffe2326c84
Shubh Pakale <shubh,pakale28@gmail.com>
2025-12-28 18:10:45 +0530

    Giving the context to the model , to answer my further question

--- First.py
+++ First.py
@@ -1,12 +1,25 @@
 from google import genai
 
-# Store the API key in a variable
-api_key = "AIzaSyDMBCJtnR5D8s6i-DQSgF-6Wg8AWZWFhNc"  # Replace with your actual API key
+api_key = "AIzaSyDMBCJtnR5D8s6i-DQSgF-6Wg8AWZWFhNc"
 
 # Pass the API key to the client
 client = genai.Client(api_key=api_key)
 
 response = client.models.generate_content(
-    model="gemini-2.5-flash", contents="Explain how machine learning works in a few words"
+    model="gemini-2.5-flash", 
+    contents= [
+        {
+            "role": "user",
+            "parts": [{"text": "Hi I am Shubh" }]
+        },
+        {
+            "role": "model",
+            "parts": [{"text": "Hi Shubh! It's nice to meet you. How can I help you today?" }]
+        },
+        {
+            "role": "user",
+            "parts": [{"text": "What is my Name?" }]
+        }
+    ]
 )
 print(response.text)
